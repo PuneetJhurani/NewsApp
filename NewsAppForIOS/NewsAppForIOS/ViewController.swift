105d4118c6097c5d31cd029a6efae8edb1847001
Puneet jhurani <puneet.jhurani@nagarro.com>
2020-07-13 18:46:59 +0530

    login screen
    login screen

--- NewsAppForIOS/NewsAppForIOS/ViewController.swift
+++ NewsAppForIOS/NewsAppForIOS/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.profileImageView.layer.cornerRadius  = self.profileImageView.bounds.width/2
+        self.profileImageView.layer.masksToBounds = true
+        self.profileImageView.clipsToBounds = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    override func viewDidAppear(_ animated: Bool) {
+        //
+    }
+    
+    
 
 }
 

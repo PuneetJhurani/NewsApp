c544527a4cfe403c99cdfd473bbe3e6bc2a89115
PuneetJhurani <38695873+PuneetJhurani@users.noreply.github.com>
2020-07-13 14:25:46 +0530

    new project started

---
+++ NewsAppForIOS/NewsAppForIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsAppForIOS
+//
+//  Created by Puneet jhurani  on 13/07/20.
+//  Copyright © 2020 Puneet jhurani . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
